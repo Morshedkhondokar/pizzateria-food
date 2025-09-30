802a7b9cf7dc1c091a7ef0ad7ff30ef0ed28bb81
Morshedkhondokar <morshedkhondokar99@gmail.com>
2025-09-30 13:31:34 +0600

    category button done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeHeader from "../components/home/HomeHeader";
 import Delivery from "../components/home/Delivery";
+import MenuHome from "../components/home/MenuHome";
 
 const Home = () => {
   return (
     <div>
         <HomeHeader/>
         <Delivery/>
+        <MenuHome/>
     </div>
   );
 };
